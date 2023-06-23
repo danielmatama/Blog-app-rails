d71ce7b27847894dce5fc3db4797d675f536a7d5
AmanWorku <amexworku2014@gmail.com>
2023-06-23 12:00:59 +0300

    Add index ethod to the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+
+    render json: @comments
+  end
   def new
     @comment = Comment.new
   end
