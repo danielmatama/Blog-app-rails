e1e0e7ff3e2e826df88a8c0f40ff7577fd802a3c
AmanWorku <amexworku2014@gmail.com>
2023-06-23 12:03:09 +0300

    Refactor comment displaying to use json format

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
   end
   def new
     @comment = Comment.new
+    render json: @comment
   end
 
   def create
@@ -16,11 +17,26 @@ class CommentsController < ApplicationController
     @comment.author_id = current_user.id
     @comment.post_id = params[:post_id]
     post = Post.find(params[:post_id])
-    if @comment.save
-      redirect_to user_post_path(current_user.id, post)
-    else
-      render :new
-    end
+    # if @comment.save
+    #   redirect_to user_post_path(current_user.id, post)
+    # else
+    #   render :new
+    # end
+    respond_to do |format|
+      if @comment.save
+        format.html do
+          flash[:success] = 'Comment created successfully'
+          redirect_to user_post_url(@post.author, @post)
+        end
+        format.json { render json: @comment, status: :created }
+      else
+        format.html do
+          flash[:error] = 'Error: Comment not created'
+          redirect_to new_user_post_comment_url(@post.author, @post)
+        end
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+
   end
 
   def destroy
