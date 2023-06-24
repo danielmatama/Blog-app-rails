e392ad348839e616089a94122c83076720f5dd5c
daniel matama <danmatama2020@gmail.com>
2023-06-24 12:31:44 +0300

    create documentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,13 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
+    # @posts = Post.where(author_id: @user)
     @posts = @user.posts.includes(:comments, :likes)
-    render json: @posts
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    render json: @post
   end
 
   def new
